917a5d463ea2e2c463b335b4290c386a419715bb
Fasi Owaiz Ahmed <fasiowaizahmed1234@gmail.com>
2026-03-15 13:50:27 +0530

    inngest demo working

---
+++ src/app/api/demo/background/route.ts
@@ -0,0 +1,14 @@
+//POST localhost3000/api/demo/background
+
+import { generateText } from 'ai';
+import {google} from "@ai-sdk/google"
+import { inngest } from '@/inngest/client';
+
+
+
+export async function POST(){
+    await inngest.send({
+        name: "demo/generate",
+        data: {}
+    })
+}
--- src/app/demo/page.tsx
+++ src/app/demo/page.tsx
@@ -7,6 +7,13 @@ import { useState } from 'react'
 
 export default function DemoPage() {
     const [loading, setLoading] = useState(false)
+    const [loading2, setLoading2] = useState(false)
+
+    const handleBackground = async()=>{
+        setLoading2(true)
+        await fetch("/api/demo/background", {method:"POST"})
+        setLoading2(false)
+    }
 
     const handleBlocking = async ()=>{
         setLoading(true)
@@ -18,6 +25,10 @@ export default function DemoPage() {
     <div className="p-8 space-x-4">
         <Button disabled={loading} onClick={handleBlocking}>
             {loading? "Loading...": "Blocking"}
+        </Button>
+
+        <Button disabled={loading2} onClick={handleBackground}>
+            {loading2? "Loading...": "Background"}
         </Button>
     </div>
   )
