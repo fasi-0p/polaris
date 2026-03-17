d3518c4f564dec5ded4b3792cc11d581956d1003
Fasi Owaiz Ahmed <fasiowaizahmed1234@gmail.com>
2026-03-17 13:44:51 +0530

    projects layout and backend done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,9 @@
 'use client'
-import {useMutation} from "convex/react";
-import { useQuery } from "convex/react";
-import { api } from "../../convex/_generated/api";
-import {Button} from "@/components/ui/button";
+import {ProjectsView} from "@/features/projects/components/projects-view";
 
-const X = () => {
-  const projects = useQuery(api.projects.get);
-  const createProject = useMutation(api.projects.create);
 
-  return (
-    <div className="flex flex-col gap-2 p-4">
-      <Button onClick={()=> createProject({
-        name: "New Project"
-      })}>
-        Add new
-      </Button>
+const Home = () => {
+  return <ProjectsView/>
+}
 
-      {projects?.map((projects) => (
-        <div
-          className="border rounded p-2 flex flex-col"
-          key={projects._id}
-        >
-          <p>{projects.name}</p>
-          <p>Owner Id: {`${projects.ownerId}`}</p>
-        </div>
-      ))}
-    </div>
-  );
-};
-
-export default X;
+export default Home;
