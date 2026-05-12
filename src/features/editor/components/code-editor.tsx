0a44ccb518827a860ffe697462ae1ecca23e219b
Fasi Owaiz Ahmed <fasiowaizahmed1234@gmail.com>
2026-05-12 23:39:46 +0530

    ide interface, codemirror integrated, adjusted for dark theme and 6 langs

---
+++ src/features/editor/components/code-editor.tsx
@@ -0,0 +1,40 @@
+import { useEffect, useMemo, useRef } from "react";
+import { basicSetup } from "codemirror";
+import { EditorView, keymap } from "@codemirror/view";
+import { oneDark } from "@codemirror/theme-one-dark";
+import { indentWithTab } from "@codemirror/commands";
+import { customTheme } from "../extensions/theme";
+import { getLanguageExtension } from "../extensions/language-extension";
+
+interface Props{
+    fileName:string;
+}
+
+export const CodeEditor = ({fileName}:Props) => {
+  const editorRef = useRef<HTMLDivElement>(null);
+  const viewRef = useRef<EditorView | null>(null);
+
+  const languageExtension=useMemo(()=> getLanguageExtension(fileName), [fileName])
+
+  useEffect(() => {
+    if(!editorRef.current) return;
+
+    const view = new EditorView({
+      doc: "Start document",
+      parent: editorRef.current,
+      extensions:[oneDark, customTheme, basicSetup,languageExtension, keymap.of([indentWithTab])]
+    });
+    viewRef.current = view;
+
+    return () => {
+      view.destroy();
+    };
+  }, []);
+
+  return (
+    <div
+      ref={editorRef}
+      className="size-full pl-4 bg-background"
+    />
+  );
+};
--- src/features/editor/components/editor-view.tsx
+++ src/features/editor/components/editor-view.tsx
@@ -1,18 +1,44 @@
-import {Id} from "../../../../convex/_generated/dataModel"
-import {TopNavigation} from "./top-navigation"
-import {FileBreadcrumbs} from "./file-breadcrumbs"
-import {useEditor} from "@/features/editor/hooks/use-editor"
+import { Id } from "../../../../convex/_generated/dataModel";
+import Image from "next/image";
 
+import { TopNavigation } from "./top-navigation";
+import { FileBreadcrumbs } from "./file-breadcrumbs";
+import { CodeEditor } from "./code-editor";
 
-export const EditorView=({projectId}:{projectId: Id<"projects">})=>{
-    const {activeTabId}=useEditor(projectId)
+import { useEditor } from "@/features/editor/hooks/use-editor";
+import { useFile } from "@/features/projects/hooks/use-files";
 
-    return (
-        <div className='h-full flex flex-col'>
-            <div className='flex items-center'>
-                <TopNavigation projectId={projectId}/>
-            </div>
-            {activeTabId &&<FileBreadcrumbs projectId={projectId}/>}
-        </div>
-    )
+interface EditorViewProps {
+  projectId: Id<"projects">;
 }
+
+export const EditorView = ({ projectId }: EditorViewProps) => {
+  const { activeTabId } = useEditor(projectId);
+  const activeFile = useFile(activeTabId);
+
+  return (
+    <div className="h-full flex flex-col">
+      <div className="flex items-center">
+        <TopNavigation projectId={projectId} />
+      </div>
+
+      {activeTabId && <FileBreadcrumbs projectId={projectId} />}
+
+      <div className="flex-1 min-h-0 bg-background">
+        {!activeFile ? (
+          <div className="size-full flex items-center justify-center">
+            <Image
+              src="/vercel.svg"
+              alt="logo"
+              width={50}
+              height={50}
+              className="opacity-25"
+            />
+          </div>
+        ) : (
+          <CodeEditor fileName={activeFile.name} />
+        )}
+      </div>
+    </div>
+  );
+};
